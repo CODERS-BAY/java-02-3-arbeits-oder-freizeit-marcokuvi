2eab7dbb5369cf640889314b408bc3b3b62d20ad
Marco <kuvimarco@gmail.com>
2020-08-31 09:58:51 +0200

    Add FreeTimeOrWorkTime Class

--- src/FreeTimeOrWorkTime.java
+++ src/FreeTimeOrWorkTime.java
@@ -9,9 +9,9 @@ public class FreeTimeOrWorkTime {
 
         if (hour == 12 ) {
             System.out.println("Free Time");
-        } else if (hour < 12 || hour > 8) {
+        } else if (hour > 8 && hour < 12) {
             System.out.println("Work Time");
-        } else if (hour < 16 || hour > 13){
+        } else if (hour > 13 && hour < 16){
             System.out.println("Work Time");
         } else {
             System.out.println("Free Time");
