d752a88f8a3eb05ba3283787f3570cee424f18cf
Marco <kuvimarco@gmail.com>
2020-08-31 11:31:56 +0200

    Add FreeTimeOrWorkTime Class

--- src/FreeTimeOrWorkTime.java
+++ src/FreeTimeOrWorkTime.java
@@ -2,16 +2,16 @@
 import java.util.Scanner;
 
 public class FreeTimeOrWorkTime {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Time:");
         int hour = scan.nextInt();
 
-        if (hour == 12 ) {
+        if (hour == 12) {
             System.out.println("Free Time");
         } else if (hour > 8 && hour < 12) {
             System.out.println("Work Time");
-        } else if (hour > 13 && hour < 16){
+        } else if (hour > 13 && hour < 16) {
             System.out.println("Work Time");
         } else {
             System.out.println("Free Time");
